2f22ce42e89da90cbb7955b87e72457aa7ef32ec
zahraaad227 <zehraadgozelova@gmail.com>
2025-03-12 11:37:58 +0400

    Create lab2.py
    labaratoriya 2 ci is

---
+++ lab2.py
@@ -0,0 +1,11 @@
+b= int(input("b= "))
+a = int(input("a= "))
+x=0
+while x<=1:
+    if b-2*a<2:
+        print(2(a**2)+b)
+    elif b-2*a=2:
+        print(8*a*x-2*b*c)
+    else:
+      print(3*b-2*a)
+    x+=0.1
